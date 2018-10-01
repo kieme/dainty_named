f406c56d1c3ac3539702fe34f5c1386b6d1a1a28
kieme <frits.germs@gmx.net>
2018-10-01 09:49:18 +0200

    explicitly define all the internal types. don't require t_prefix then.

--- dainty_named.h
+++ dainty_named.h
@@ -172,55 +172,230 @@ namespace named
 ///////////////////////////////////////////////////////////////////////////////
 
   using t_bool         = t_prefix<bool>::t_;
+  using r_bool         = t_prefix<t_bool>::r_;
+  using R_bool         = t_prefix<t_bool>::R_;
+  using p_bool         = t_prefix<t_bool>::p_;
+  using P_bool         = t_prefix<t_bool>::P_;
+
   using t_char         = t_prefix<char>::t_;
+  using r_char         = t_prefix<t_char>::r_;
+  using R_char         = t_prefix<t_char>::R_;
+  using p_char         = t_prefix<t_char>::p_;
+  using P_char         = t_prefix<t_char>::P_;
+
   using t_uchar        = t_prefix<unsigned char>::t_;
+  using r_uchar        = t_prefix<t_uchar>::r_;
+  using R_uchar        = t_prefix<t_uchar>::R_;
+  using p_uchar        = t_prefix<t_uchar>::p_;
+  using P_uchar        = t_prefix<t_uchar>::P_;
+
   using t_int          = t_prefix<int>::t_;
+  using r_int          = t_prefix<t_int>::r_;
+  using R_int          = t_prefix<t_int>::R_;
+  using p_int          = t_prefix<t_int>::p_;
+  using P_int          = t_prefix<t_int>::P_;
+
   using t_uint         = t_prefix<unsigned int>::t_;
+  using r_uint         = t_prefix<t_uint>::r_;
+  using R_uint         = t_prefix<t_uint>::R_;
+  using p_uint         = t_prefix<t_uint>::p_;
+  using P_uint         = t_prefix<t_uint>::P_;
+
   using t_short        = t_prefix<short>::t_;
+  using r_short        = t_prefix<t_short>::r_;
+  using R_short        = t_prefix<t_short>::R_;
+  using p_short        = t_prefix<t_short>::p_;
+  using P_short        = t_prefix<t_short>::P_;
+
   using t_ushort       = t_prefix<unsigned short>::t_;
+  using r_ushort       = t_prefix<t_ushort>::r_;
+  using R_ushort       = t_prefix<t_ushort>::R_;
+  using p_ushort       = t_prefix<t_ushort>::p_;
+  using P_ushort       = t_prefix<t_ushort>::P_;
+
   using t_long         = t_prefix<long int>::t_;
+  using r_long         = t_prefix<t_long>::r_;
+  using R_long         = t_prefix<t_long>::R_;
+  using p_long         = t_prefix<t_long>::p_;
+  using P_long         = t_prefix<t_long>::P_;
+
   using t_ulong        = t_prefix<unsigned long int>::t_;
+  using r_ulong        = t_prefix<t_ulong>::r_;
+  using R_ulong        = t_prefix<t_ulong>::R_;
+  using p_ulong        = t_prefix<t_ulong>::p_;
+  using P_ulong        = t_prefix<t_ulong>::P_;
+
   using t_llong        = t_prefix<long long int>::t_;
+  using r_llong        = t_prefix<t_llong>::r_;
+  using R_llong        = t_prefix<t_llong>::R_;
+  using p_llong        = t_prefix<t_llong>::p_;
+  using P_llong        = t_prefix<t_llong>::P_;
+
   using t_ullong       = t_prefix<unsigned long long int>::t_;
+  using r_ullong       = t_prefix<t_ullong>::r_;
+  using R_ullong       = t_prefix<t_ullong>::R_;
+  using p_ullong       = t_prefix<t_ullong>::p_;
+  using P_ullong       = t_prefix<t_ullong>::P_;
+
   using t_double       = t_prefix<double>::t_;
+  using r_double       = t_prefix<t_double>::r_;
+  using R_double       = t_prefix<t_double>::R_;
+  using p_double       = t_prefix<t_double>::p_;
+  using P_double       = t_prefix<t_double>::P_;
+
   using t_void         = t_prefix<void>::t_;
+  using p_void         = t_prefix<t_void>::p_;
+  using P_void         = t_prefix<t_void>::P_;
 
   using t_int8         = t_prefix<std::int8_t>::t_;
+  using r_int8         = t_prefix<t_int8>::r_;
+  using R_int8         = t_prefix<t_int8>::R_;
+  using p_int8         = t_prefix<t_int8>::p_;
+  using P_int8         = t_prefix<t_int8>::P_;
+
   using t_int16        = t_prefix<std::int16_t>::t_;
+  using r_int16        = t_prefix<t_int16>::r_;
+  using R_int16        = t_prefix<t_int16>::R_;
+  using p_int16        = t_prefix<t_int16>::p_;
+  using P_int16        = t_prefix<t_int16>::P_;
+
   using t_int32        = t_prefix<std::int32_t>::t_;
+  using r_int32        = t_prefix<t_int32>::r_;
+  using R_int32        = t_prefix<t_int32>::R_;
+  using p_int32        = t_prefix<t_int32>::p_;
+  using P_int32        = t_prefix<t_int32>::P_;
+
   using t_int64        = t_prefix<std::int64_t>::t_;
+  using r_int64        = t_prefix<t_int64>::r_;
+  using R_int64        = t_prefix<t_int64>::R_;
+  using p_int64        = t_prefix<t_int64>::p_;
+  using P_int64        = t_prefix<t_int64>::P_;
 
   using t_uint8        = t_prefix<std::uint8_t>::t_;
+  using r_uint8        = t_prefix<t_uint8>::r_;
+  using R_uint8        = t_prefix<t_uint8>::R_;
+  using p_uint8        = t_prefix<t_uint8>::p_;
+  using P_uint8        = t_prefix<t_uint8>::P_;
+
   using t_uint16       = t_prefix<std::uint16_t>::t_;
+  using r_uint16       = t_prefix<t_uint16>::r_;
+  using R_uint16       = t_prefix<t_uint16>::R_;
+  using p_uint16       = t_prefix<t_uint16>::p_;
+  using P_uint16       = t_prefix<t_uint16>::P_;
+
   using t_uint32       = t_prefix<std::uint32_t>::t_;
+  using r_uint32       = t_prefix<t_uint32>::r_;
+  using R_uint32       = t_prefix<t_uint32>::R_;
+  using p_uint32       = t_prefix<t_uint32>::p_;
+  using P_uint32       = t_prefix<t_uint32>::P_;
+
   using t_uint64       = t_prefix<std::uint64_t>::t_;
+  using r_uint64       = t_prefix<t_uint64>::r_;
+  using R_uint64       = t_prefix<t_uint64>::R_;
+  using p_uint64       = t_prefix<t_uint64>::p_;
+  using P_uint64       = t_prefix<t_uint64>::P_;
 
   using t_int_least8   = t_prefix<std::int_least8_t>::t_;
+  using r_int_least8   = t_prefix<t_int_least8>::r_;
+  using R_int_least8   = t_prefix<t_int_least8>::R_;
+  using p_int_least8   = t_prefix<t_int_least8>::p_;
+  using P_int_least8   = t_prefix<t_int_least8>::P_;
+
   using t_int_least16  = t_prefix<std::int_least16_t>::t_;
+  using r_int_least16  = t_prefix<t_int_least16>::r_;
+  using R_int_least16  = t_prefix<t_int_least16>::R_;
+  using p_int_least16  = t_prefix<t_int_least16>::p_;
+  using P_int_least16  = t_prefix<t_int_least16>::P_;
+
   using t_int_least32  = t_prefix<std::int_least32_t>::t_;
+  using r_int_least32  = t_prefix<t_int_least32>::r_;
+  using R_int_least32  = t_prefix<t_int_least32>::R_;
+  using p_int_least32  = t_prefix<t_int_least32>::p_;
+  using P_int_least32  = t_prefix<t_int_least32>::P_;
+
   using t_int_least64  = t_prefix<std::int_least64_t>::t_;
+  using r_int_least64  = t_prefix<t_int_least64>::r_;
+  using R_int_least64  = t_prefix<t_int_least64>::R_;
+  using p_int_least64  = t_prefix<t_int_least64>::p_;
+  using P_int_least64  = t_prefix<t_int_least64>::P_;
 
   using t_uint_least8  = t_prefix<std::uint_least8_t>::t_;
+  using r_uint_least8  = t_prefix<t_uint_least8>::r_;
+  using R_uint_least8  = t_prefix<t_uint_least8>::R_;
+  using p_uint_least8  = t_prefix<t_uint_least8>::p_;
+  using P_uint_least8  = t_prefix<t_uint_least8>::P_;
+
   using t_uint_least16 = t_prefix<std::uint_least16_t>::t_;
+  using r_uint_least16 = t_prefix<t_uint_least16>::r_;
+  using R_uint_least16 = t_prefix<t_uint_least16>::R_;
+  using p_uint_least16 = t_prefix<t_uint_least16>::p_;
+  using P_uint_least16 = t_prefix<t_uint_least16>::P_;
+
   using t_uint_least32 = t_prefix<std::uint_least32_t>::t_;
+  using r_uint_least32 = t_prefix<t_uint_least32>::r_;
+  using R_uint_least32 = t_prefix<t_uint_least32>::R_;
+  using p_uint_least32 = t_prefix<t_uint_least32>::p_;
+  using P_uint_least32 = t_prefix<t_uint_least32>::P_;
+
   using t_uint_least64 = t_prefix<std::uint_least64_t>::t_;
+  using r_uint_least64 = t_prefix<t_uint_least64>::r_;
+  using R_uint_least64 = t_prefix<t_uint_least64>::R_;
+  using p_uint_least64 = t_prefix<t_uint_least64>::p_;
+  using P_uint_least64 = t_prefix<t_uint_least64>::P_;
 
   using t_int_fast8    = t_prefix<std::int_fast8_t>::t_;
+  using r_int_fast8    = t_prefix<t_int_fast8>::r_;
+  using R_int_fast8    = t_prefix<t_int_fast8>::R_;
+  using p_int_fast8    = t_prefix<t_int_fast8>::p_;
+  using P_int_fast8    = t_prefix<t_int_fast8>::P_;
+
   using t_int_fast16   = t_prefix<std::int_fast16_t>::t_;
+  using r_int_fast16   = t_prefix<t_int_fast16>::r_;
+  using R_int_fast16   = t_prefix<t_int_fast16>::R_;
+  using p_int_fast16   = t_prefix<t_int_fast16>::p_;
+  using P_int_fast16   = t_prefix<t_int_fast16>::P_;
+
   using t_int_fast32   = t_prefix<std::int_fast32_t>::t_;
+  using r_int_fast32   = t_prefix<t_int_fast32>::r_;
+  using R_int_fast32   = t_prefix<t_int_fast32>::R_;
+  using p_int_fast32   = t_prefix<t_int_fast32>::p_;
+  using P_int_fast32   = t_prefix<t_int_fast32>::P_;
+
   using t_int_fast64   = t_prefix<std::int_fast64_t>::t_;
+  using r_int_fast64   = t_prefix<t_int_fast64>::r_;
+  using R_int_fast64   = t_prefix<t_int_fast64>::R_;
+  using p_int_fast64   = t_prefix<t_int_fast64>::p_;
+  using P_int_fast64   = t_prefix<t_int_fast64>::P_;
 
   using t_uint_fast8   = t_prefix<std::uint_fast8_t>::t_;
+  using r_uint_fast8   = t_prefix<t_uint_fast8>::r_;
+  using R_uint_fast8   = t_prefix<t_uint_fast8>::R_;
+  using p_uint_fast8   = t_prefix<t_uint_fast8>::p_;
+  using P_uint_fast8   = t_prefix<t_uint_fast8>::P_;
+
   using t_uint_fast16  = t_prefix<std::uint_fast16_t>::t_;
+  using r_uint_fast16  = t_prefix<t_uint_fast16>::r_;
+  using R_uint_fast16  = t_prefix<t_uint_fast16>::R_;
+  using p_uint_fast16  = t_prefix<t_uint_fast16>::p_;
+  using P_uint_fast16  = t_prefix<t_uint_fast16>::P_;
+
   using t_uint_fast32  = t_prefix<std::uint_fast32_t>::t_;
+  using r_uint_fast32  = t_prefix<t_uint_fast32>::r_;
+  using R_uint_fast32  = t_prefix<t_uint_fast32>::R_;
+  using p_uint_fast32  = t_prefix<t_uint_fast32>::p_;
+  using P_uint_fast32  = t_prefix<t_uint_fast32>::P_;
+
   using t_uint_fast64  = t_prefix<std::uint_fast64_t>::t_;
+  using r_uint_fast64  = t_prefix<t_uint_fast64>::r_;
+  using R_uint_fast64  = t_prefix<t_uint_fast64>::R_;
+  using p_uint_fast64  = t_prefix<t_uint_fast64>::p_;
+  using P_uint_fast64  = t_prefix<t_uint_fast64>::P_;
 
-  using t_uintptr  = t_prefix<std::uintptr_t>::t_;
-
-///////////////////////////////////////////////////////////////////////////////
-
-  using p_void = t_prefix<t_void>::p_;
-  using P_void = t_prefix<t_void>::P_;
+  using t_uintptr      = t_prefix<std::uintptr_t>::t_;
+  using r_uintptr      = t_prefix<t_uintptr>::r_;
+  using R_uintptr      = t_prefix<t_uintptr>::R_;
+  using p_uintptr      = t_prefix<t_uintptr>::p_;
+  using P_uintptr      = t_prefix<t_uintptr>::P_;
 
 ///////////////////////////////////////////////////////////////////////////////
 
